2c50351fe58c9c05f8d217652f047e9b873ce879
Victor Ndaba <mwangindaba@gmail.com>
2022-10-07 22:16:10 +0300

    feat: created basic express app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express"
+import morgan from "morgan"
+import helmet from "helmet"
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(helmet())
+app.use(morgan("combined"));
+
+export default app;
